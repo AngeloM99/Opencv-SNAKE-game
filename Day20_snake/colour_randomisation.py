c2b68502a7f59d7e33fe791aad345ebe28ca7316
AngeloM99 <72969725+AngeloM99@users.noreply.github.com>
2021-10-13 13:41:24 +1100

    Delete Day20_snake directory

--- Day20_snake/colour_randomisation.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def color_randomisation():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
